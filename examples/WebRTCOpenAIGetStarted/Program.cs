865e76394f6ce6f642668a45935d0c3dd2a4454c
Aaron Clauson <aaron@sipsorcery.com>
2025-05-27 23:13:12 +0100

    Added migrated example programs.

---
+++ examples/WebRTCOpenAIGetStarted/Program.cs
@@ -0,0 +1,117 @@
+﻿//-----------------------------------------------------------------------------
+// Filename: Program.cs
+//
+// Description: An example WebRTC application that can be used to interact with
+// OpenAI's real-time API https://platform.openai.com/docs/guides/realtime-webrtc.
+//
+// NOTE: As of 10 May 2025 this example does work to establish an audio stream and is
+// able to receive data channel messages. There is no echo cancellation feature in this
+// demo so if not provided by the by your Windows audio device then ChatGPT will end
+// up talking to itself (as a workaround use a headset).
+//
+// Usage:
+// set OPENAIKEY=your_openai_key
+// dotnet run
+//
+// Author(s):
+// Aaron Clauson (aaron@sipsorcery.com)
+// 
+// History:
+// 19 Dec 2024	Aaron Clauson	Created, Dublin, Ireland.
+// 17 Jan 2025  Aaron Clauson   Added create resposne data channel message to trigger conversation start.
+// 10 May 2025  Aaron Clauson   Big refactor of the OpenAI.Realtime library to use HttpClientFactory.
+// 27 May 2025  Aaron Clauson   Moved from SIPSorcery main repo to SIPSorcery.OpenAI.WebRTC repo.
+//
+// License: 
+// BSD 3-Clause "New" or "Revised" License and the additional
+// BDS BY-NC-SA restriction, see included LICENSE.md file.
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Serilog;
+using SIPSorceryMedia.Windows;
+using Serilog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using SIPSorcery.OpenAI.WebRTC;
+using SIPSorceryMedia.Abstractions;
+using SIPSorcery.Media;
+
+namespace demo;
+
+class Program
+{
+    static async Task Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var loggerFactory = new SerilogLoggerFactory(Log.Logger);
+        SIPSorcery.LogFactory.Set(loggerFactory);
+
+        Log.Logger.Information("WebRTC OpenAI Demo Program");
+
+        var openAiKey = Environment.GetEnvironmentVariable("OPENAIKEY") ?? string.Empty;
+
+        if (string.IsNullOrWhiteSpace(openAiKey))
+        {
+            Log.Logger.Error("Please provide your OpenAI key as an environment variable. For example: set OPENAIKEY=<your openai api key>");
+            return;
+        }
+
+        var logger = loggerFactory.CreateLogger<Program>();
+        var webrtcEndPoint = new WebRTCEndPoint(openAiKey, logger);
+
+        // We'll send/receive audio directly from our Windows audio devices.
+        InitialiseWindowsAudioEndPoint(webrtcEndPoint, Log.Logger);
+
+        var negotiateConnectResult = await webrtcEndPoint.StartConnectAsync();
+
+        if(negotiateConnectResult.IsLeft)
+        {
+            Log.Logger.Error($"Failed to negotiation connection to OpenAI Realtime WebRTC endpoint: {negotiateConnectResult.LeftAsEnumerable().First()}");
+            return;
+        }
+
+        webrtcEndPoint.OnPeerConnectionConnected += () =>
+        {
+            Log.Logger.Information("WebRTC peer connection established.");
+
+            // Trigger the conversation by sending a response create message.
+            var result = webrtcEndPoint.SendResponseCreate(OpenAIVoicesEnum.shimmer, "Say Hi!");
+            if (result.IsLeft)
+            {
+                Log.Logger.Error($"Failed to send response create message: {result.LeftAsEnumerable().First()}");
+            }
+        };
+
+        webrtcEndPoint.OnDataChannelMessageReceived += (dc, message) =>
+        {
+            if (message is OpenAIResponseAudioTranscriptDone done)
+            {
+                Log.Information($"Transcript done: {done.Transcript}");
+            }
+        };
+
+        Console.WriteLine("Wait for ctrl-c to indicate user exit.");
+
+        var exitTcs = new TaskCompletionSource<object?>();
+        Console.CancelKeyPress += (s, e) =>
+        {
+            e.Cancel = true;
+            exitTcs.TrySetResult(null);
+        };
+
+        await exitTcs.Task;
+    }
+
+    private static void InitialiseWindowsAudioEndPoint(IWebRTCEndPoint webrtcEndPoint, Serilog.ILogger logger)
+    {
+        var audioEncoder = new AudioEncoder(AudioCommonlyUsedFormats.OpusWebRTC);
+        WindowsAudioEndPoint windowsAudioEP = new WindowsAudioEndPoint(audioEncoder);
+        webrtcEndPoint.ConnectAudioEndPoint(windowsAudioEP);
+    }
+}
